348b8bb24c1a59ed28e8d3a7786deabe27f1d6ef
lishimeng <alex.li@jxfinancial.cn>
2019-07-30 18:20:53 +0800

    shutdown

---
+++ shutdown/shutdown.go
@@ -0,0 +1,52 @@
+package shutdown
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type Configuration struct {
+	BeforeExit func(string)
+	AuthShutdownTime time.Duration
+	Signals []os.Signal
+}
+
+var defaultSignals = []os.Signal{ syscall.SIGINT, syscall.SIGTERM }
+
+func WaitExit(config *Configuration) {
+
+	sigChan := make(chan os.Signal)
+	exitChan := make(chan struct{message string})
+	if config != nil {
+		if len(config.Signals) > 0 {
+			defaultSignals = config.Signals
+		}
+	}
+
+	signal.Notify(sigChan, defaultSignals...)
+
+	go func() {
+		if config != nil && config.AuthShutdownTime > time.Second {
+			time.Sleep(config.AuthShutdownTime)
+			exitChan <- struct{message string}{"auto"}
+		}
+	}()
+	select {
+	case s := <-exitChan:
+		onExit(s.message, config)
+	case s := <-sigChan:
+		onExit(s.String(), config)
+	}
+}
+
+func onExit(s string, config *Configuration) {
+
+	defer func() {
+		recover()
+	}()
+	if config != nil && config.BeforeExit != nil {
+		config.BeforeExit(s)
+	}
+}
