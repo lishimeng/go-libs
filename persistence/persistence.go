bb16624b4ce903770ff4a72586a0ffeb8c113fe2
lishimeng <alex.li@jxfinancial.cn>
2019-12-31 17:12:48 +0800

    c

--- persistence/persistence.go
+++ persistence/persistence.go
@@ -7,6 +7,15 @@ import (
 //RegisterModel
 type DriverType orm.DriverType
 
+const (
+	_          DriverType = iota // int enum type
+	DRMySQL                      // mysql
+	DRSqlite                     // sqlite
+	DROracle                     // oracle
+	DRPostgres                   // pgsql
+	DRTiDB                       // TiDB
+)
+
 func RegisterDriver(driver string, t DriverType) (err error) {
 	err = orm.RegisterDriver(driver, orm.DriverType(t))
 	return
