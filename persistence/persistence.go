964f91fb19b513b1f0bec1d5a59c495aa9dd8b81
lishimeng <alex.li@jxfinancial.cn>
2019-12-31 17:28:44 +0800

    c

--- persistence/persistence.go
+++ persistence/persistence.go
@@ -13,10 +13,10 @@ type Driver struct {
 }
 
 var DriverMysql = Driver{"mysql", orm.DRMySQL}
-var DriverSqlite = Driver{"mysql", orm.DRSqlite}
-var DriverOracle = Driver{"mysql", orm.DROracle}
-var DriverPostgres = Driver{"mysql", orm.DRPostgres}
-var DriverTiDB = Driver{"mysql", orm.DRTiDB}
+var DriverSqlite = Driver{"sqlite3", orm.DRSqlite}
+var DriverOracle = Driver{"oracle", orm.DROracle}
+var DriverPostgres = Driver{"postgres", orm.DRPostgres}
+var DriverTiDB = Driver{"tidb", orm.DRTiDB}
 
 func RegisterDriver(driver Driver) (err error) {
 	err = orm.RegisterDriver(driver.name, driver.t)
